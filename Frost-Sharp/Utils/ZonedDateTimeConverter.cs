6a9e00d38d2c57479b6127e4e4a9861ddc4c689c
DirtyDan1 <dirtydan@national.shitposting.agency>
2017-03-31 18:19:32 +0100

    Add missing ZonedDateTimeConverter.cs

---
+++ Frost-Sharp/Utils/ZonedDateTimeConverter.cs
@@ -0,0 +1,22 @@
+﻿using NodaTime;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Data;
+
+namespace Frost_Sharp.Utils {
+	public class ZonedDateTimeConverter : IValueConverter {
+		public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture) {
+			ZonedDateTime? zdt = value as ZonedDateTime?;
+			if (zdt.HasValue) {
+				return Utils.FormatDate(zdt.Value);
+			}
+			return null;
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture) {
+			throw new NotImplementedException();
+		}
+	}
+}
